c215c357c0df13533e8bf8213c696398652d9503
raynor <kykim@artificial.sc>
2025-07-08 11:33:13 +0900

    update

--- quantize/autoround.py
+++ quantize/autoround.py
@@ -4,6 +4,7 @@ from auto_round import AutoRound
 model_name = "skt/A.X-4.0"
 print(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto")
+model = torch.compile(model) # torch compile
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 bits, group_size, sym = 2, 128, True
@@ -12,7 +13,7 @@ bits, group_size, sym = 2, 128, True
 # autoround = AutoRound(model, tokenizer, bits=bits, group_size=group_size, sym=sym)
 
 ## the best accuracy, 4-5X slower, low_gpu_mem_usage could save ~20G but ~30% slower
-autoround = AutoRound(model, tokenizer, nsamples=512, iters=1000, low_gpu_mem_usage=False, bits=bits, group_size=group_size, sym=sym)
+autoround = AutoRound(model, tokenizer, nsamples=512, iters=1000, low_gpu_mem_usage=True, bits=bits, group_size=group_size, sym=sym)
 
 ## 2-3X speedup, slight accuracy drop at W4G128
 # autoround = AutoRound(model, tokenizer, nsamples=128, iters=50, lr=5e-3, bits=bits, group_size=group_size, sym=sym )
